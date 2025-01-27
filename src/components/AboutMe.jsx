e24ed10be051cb63222df943e78e5ee57b2f2acf
Luis Cortes <71467212+eligio12@users.noreply.github.com>
2025-01-27 10:38:42 -0600

    Cambio en la edad

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -23,7 +23,7 @@ const AboutMe = () => {
           <div className="information">
             <h3 className="information__title"><svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M436 160c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v416c0 26.5 21.5 48 48 48h320c26.5 0 48-21.5 48-48v-48h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20c6.6 0 12-5.4 12-12v-40c0-6.6-5.4-12-12-12h-20v-64h20zm-228-32c35.3 0 64 28.7 64 64s-28.7 64-64 64-64-28.7-64-64 28.7-64 64-64zm112 236.8c0 10.6-10 19.2-22.4 19.2H118.4C106 384 96 375.4 96 364.8v-19.2c0-31.8 30.1-57.6 67.2-57.6h5c12.3 5.1 25.7 8 39.8 8s27.6-2.9 39.8-8h5c37.1 0 67.2 25.8 67.2 57.6v19.2z"></path></svg> Información</h3>
             <ul className="myInformation">
-              <li className="datum"><span>Edad: </span>25</li>
+              <li className="datum"><span>Edad: </span>27</li>
               <li className="datum"><span>Celular: </span>+52 467-102-8465</li>
               <li className="datum"><span>Ubicación: </span>Guadalajara, Jalisco, México</li>
               <li className="email"><span>Email: </span>luis.cortes7593@alumnos.udg.mx</li>
